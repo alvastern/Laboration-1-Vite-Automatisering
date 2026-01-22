74e1a21d2ab0b2cfb2da955ff0d150c8670c3f01
alvastern <alva.stern02@gmail.com>
2026-01-22 11:42:50 +0100

    Lade till funktion i JS - se vart man befinner sig på webbplatsen

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,21 @@
 "use strict"
 
+
+// Skapar möjlighet för hamburgermenyn
 let menu = document.querySelector(".menu-toggle");
 let nav = document.querySelector("header nav");
 
 menu.addEventListener("click", () => {
     nav.classList.toggle('open');
+});
+
+
+// Visar vilken sida användaren befinner sig på
+let navLinks = document.querySelectorAll("header nav a");
+let currentPlace = window.location.href;
+
+navLinks.forEach(link => {
+    if(link.href === currentPlace) {
+        link.classList.add("active");
+    }
 });
